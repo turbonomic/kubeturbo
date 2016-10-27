bf75ad8362a885f811b2ce764c06a108e27730a8
Dongyi Yang <dongyi.yang@vmturbo.com>
2016-10-27 17:20:32 -0400

    Changed run_kubeturbo.sh

--- examples/run_kubeturbo.sh
+++ examples/run_kubeturbo.sh
@@ -2,7 +2,7 @@ OUTPUT_DIR=${OUTPUT_DIR:-"_output"}
 
 sudo -E "./${OUTPUT_DIR}/kubeturbo" \
 	 --v=3 \
-	 --master="http://127.0.0.1:8080" \
+	 --master="http://172.17.0.1:8080" \
 	 --etcd-servers="http://127.0.0.1:2379" \
 	 --flag-path="$GOPATH/src/github.com/vmturbo/kubeturbo/examples/vmt_testing_flag.json" \
 	 --config-path="$GOPATH/src/github.com/vmturbo/kubeturbo/examples/vmt_testing_config.json" > "/tmp/kubeturbo.log" 2>&1 &
