af6a876a31618be1d3b16b463462175bf73c06da
max_wang <max.wang@vmturbo.com>
2018-02-01 11:42:28 -0500

    Fix race condition in kubeturbo_builder_test.go

--- cmd/kubeturbo/app/kubeturbo_builder_test.go
+++ cmd/kubeturbo/app/kubeturbo_builder_test.go
@@ -2,26 +2,44 @@ package app
 
 import (
 	"fmt"
+	"sync"
 	"syscall"
 	"testing"
 	"time"
 )
 
+type helper struct {
+	funcGotCalled bool
+}
+
+var mux sync.Mutex
+
+func (h *helper) call() {
+	mux.Lock()
+	h.funcGotCalled = true
+	mux.Unlock()
+}
+
+func (h *helper) gotCalled() bool {
+	mux.Lock()
+	defer mux.Unlock()
+	return h.funcGotCalled
+}
+
 func Test_handleExit(t *testing.T) {
-	disconnectFuncGotCalled := false
+	helper := helper{false}
 	mockDisconnectFunc := disconnectFromTurboFunc(func() {
 		fmt.Printf("Mock disconnecting process is running...")
-		disconnectFuncGotCalled = true
+		helper.call()
 	})
 
 	handleExit(mockDisconnectFunc)
-	disconnectFuncGotCalled = false
 
 	// Sending out the SIGTERM signal to trigger the disconnecting process
 	syscall.Kill(syscall.Getpid(), syscall.SIGTERM)
 	// Wait a bit for the channel to receive and process the signal
 	time.Sleep(100 * time.Millisecond)
-	if !disconnectFuncGotCalled {
+	if !helper.gotCalled() {
 		t.Errorf("The disconnect function was not invoked with signal SIGTERM")
 	}
 }
