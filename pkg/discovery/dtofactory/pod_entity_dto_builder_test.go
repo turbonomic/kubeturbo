22b2efda56f34c0ed1293469da2db3ea6207aae1
maxwangvmt <max.wang@vmturbo.com>
2018-03-15 09:27:48 -0400

    Filter out pods that are not in ready status when executing discovery (#166)
    * Filter out pods that are not in ready status when executing discovery
    * Skip entity DTO creation for pods, containers, and applications if commodity count is not correct.

---
+++ pkg/discovery/dtofactory/pod_entity_dto_builder_test.go
@@ -0,0 +1,31 @@
+package dtofactory
+
+import (
+	"testing"
+
+	"github.com/turbonomic/kubeturbo/pkg/discovery/metrics"
+	"github.com/turbonomic/turbo-go-sdk/pkg/proto"
+	api "k8s.io/client-go/pkg/api/v1"
+)
+
+var builder = &podEntityDTOBuilder{
+	generalBuilder: newGeneralBuilder(metrics.NewEntityMetricSink()),
+}
+
+func Test_podEntityDTOBuilder_getPodCommoditiesSold_Error(t *testing.T) {
+	testGetCommoditiesWithError(t, builder.getPodCommoditiesSold)
+}
+
+func Test_podEntityDTOBuilder_getPodCommoditiesBought_Error(t *testing.T) {
+	testGetCommoditiesWithError(t, builder.getPodCommoditiesBought)
+}
+
+func Test_podEntityDTOBuilder_getPodCommoditiesBoughtFromQuota_Error(t *testing.T) {
+	testGetCommoditiesWithError(t, builder.getPodCommoditiesBoughtFromQuota)
+}
+
+func testGetCommoditiesWithError(t *testing.T, f func(pod *api.Pod, cpuFrequency float64) ([]*proto.CommodityDTO, error)) {
+	if _, err := f(&api.Pod{}, 100.0); err == nil {
+		t.Errorf("Error thrown expected")
+	}
+}
