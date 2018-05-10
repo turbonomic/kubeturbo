26b317366587b88322e34a9e66b727fb6dac4f89
songbinliu <songbin.liu@turbonomic.com>
2018-05-09 22:50:06 -0400

    add action policy (#178)
    * add action policy

--- pkg/registration/registration_client.go
+++ pkg/registration/registration_client.go
@@ -1,6 +1,7 @@
 package registration
 
 import (
+	"github.com/golang/glog"
 	"github.com/turbonomic/kubeturbo/pkg/discovery/stitching"
 
 	"github.com/turbonomic/turbo-go-sdk/pkg/builder"
@@ -42,6 +43,7 @@ func (rClient *K8sRegistrationClient) GetSupplyChainDefinition() []*proto.Templa
 	supplyChainFactory := NewSupplyChainFactory(rClient.config.stitchingPropertyType, rClient.config.vmPriority, rClient.config.vmIsBase)
 	supplyChain, err := supplyChainFactory.createSupplyChain()
 	if err != nil {
+		glog.Errorf("Failed to create supply chain: %v", err)
 		// TODO error handling
 	}
 	return supplyChain
@@ -70,4 +72,47 @@ func (rClient *K8sRegistrationClient) GetAccountDefinition() []*proto.AccountDef
 
 func (rClient *K8sRegistrationClient) GetIdentifyingFields() string {
 	return TargetIdentifierField
+}
+
+func (rClient *K8sRegistrationClient) GetActionPolicy() []*proto.ActionPolicyDTO {
+	glog.V(3).Infof("Begin to build Action Policies")
+	ab := builder.NewActionPolicyBuilder()
+	supported := proto.ActionPolicyDTO_SUPPORTED
+	recommend := proto.ActionPolicyDTO_NOT_EXECUTABLE
+	notSupported := proto.ActionPolicyDTO_NOT_SUPPORTED
+
+	//1. containerPod: move, provision; not resize;
+	pod := proto.EntityDTO_CONTAINER_POD
+	podPolicy := make(map[proto.ActionItemDTO_ActionType]proto.ActionPolicyDTO_ActionCapability)
+	podPolicy[proto.ActionItemDTO_MOVE] = supported
+	podPolicy[proto.ActionItemDTO_PROVISION] = supported
+	podPolicy[proto.ActionItemDTO_RIGHT_SIZE] = notSupported
+	addActionPolicy(ab, pod, podPolicy)
+
+	//2. container: support resize; recommend provision; not move;
+	container := proto.EntityDTO_CONTAINER
+	containerPolicy := make(map[proto.ActionItemDTO_ActionType]proto.ActionPolicyDTO_ActionCapability)
+	containerPolicy[proto.ActionItemDTO_RIGHT_SIZE] = supported
+	containerPolicy[proto.ActionItemDTO_PROVISION] = recommend
+	containerPolicy[proto.ActionItemDTO_MOVE] = notSupported
+	addActionPolicy(ab, container, containerPolicy)
+
+	//3. application: only recommend provision; all else are not supported
+	app := proto.EntityDTO_APPLICATION
+	appPolicy := make(map[proto.ActionItemDTO_ActionType]proto.ActionPolicyDTO_ActionCapability)
+	appPolicy[proto.ActionItemDTO_PROVISION] = recommend
+	appPolicy[proto.ActionItemDTO_RIGHT_SIZE] = notSupported
+	appPolicy[proto.ActionItemDTO_MOVE] = notSupported
+	addActionPolicy(ab, app, appPolicy)
+
+	return ab.Create()
+}
+
+func addActionPolicy(ab *builder.ActionPolicyBuilder,
+	entity proto.EntityDTO_EntityType,
+	policies map[proto.ActionItemDTO_ActionType]proto.ActionPolicyDTO_ActionCapability) {
+
+	for action, policy := range policies {
+		ab.WithEntityActions(entity, action, policy)
+	}
 }
