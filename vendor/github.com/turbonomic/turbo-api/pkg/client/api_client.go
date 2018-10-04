72e4ee5244c0e0408b9479d3d3885c91f9221eb7
Pallavi Debnath <pallavi@debnath.org>
2018-10-04 10:53:57 -0400

    Updated with v6.2 turbo-go-sdk and turbo-api packages (#207)

--- vendor/github.com/turbonomic/turbo-api/pkg/client/api_client.go
+++ vendor/github.com/turbonomic/turbo-api/pkg/client/api_client.go
@@ -7,12 +7,18 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/golang/glog"
 	"github.com/turbonomic/turbo-api/pkg/api"
 )
 
 type Client struct {
 	*RESTClient
+	SessionCookie *http.Cookie
 }
+
+const (
+	SessionCookie string = "JSESSIONID"
+)
 
 // Create a Turbo API Client based on basic authentication.
 func NewAPIClientWithBA(c *Config) (*Client, error) {
@@ -28,7 +34,34 @@ func NewAPIClientWithBA(c *Config) (*Client, error) {
 		client = &http.Client{Transport: tr}
 	}
 	restClient := NewRESTClient(client, c.serverAddress, c.apiPath).BasicAuthentication(c.basicAuth)
-	return &Client{restClient}, nil
+	return &Client{restClient, nil}, nil
+}
+
+// Login to the Turbo API server
+func (c *Client) Login() (*Result, error) {
+	var data []byte
+	data = []byte(fmt.Sprintf("username=%s&password=%s", c.basicAuth.username, c.basicAuth.password))
+	request := c.Post().Resource("login").
+		Header("Content-Type", "application/x-www-form-urlencoded").
+		Data(data)
+
+	response, err := request.Do()
+	if err != nil {
+		return nil, fmt.Errorf("Failed to login  %s: %s", c.baseURL, err)
+	}
+	if response.statusCode != 200 {
+		return nil, buildResponseError("Turbo server login", response.status, response.body)
+	}
+
+	// Save the session cookie
+	sessionCookie, ok := response.cookies[SessionCookie]
+	if ok {
+		c.SessionCookie = sessionCookie
+		glog.V(4).Infof("Session Cookie = %s:%s\n", c.SessionCookie.Name, c.SessionCookie.Value)
+	} else {
+		return nil, buildResponseError("Invalid session cookie", response.status, fmt.Sprintf("%s", response.cookies))
+	}
+	return &response, nil
 }
 
 // Discover a target using API
@@ -45,22 +78,117 @@ func (c *Client) DiscoverTarget(uuid string) (*Result, error) {
 
 //Add a target using API
 func (c *Client) AddTarget(target *api.Target) (*Result, error) {
+	if c.SessionCookie == nil {
+		return nil, fmt.Errorf("Null login session cookie\n")
+	}
+
+	// Find if the target exists - this is a workaround since in current XL server,
+	// duplicate targets can be added
+	targetExists, _ := c.FindTarget(target)
+	if targetExists {
+		return nil, fmt.Errorf("Target %v exists", target)
+	}
+
+	glog.V(2).Infof("***************** [AddTarget] %++v\n", target)
+
+	// Create the rest api request
 	targetData, err := json.Marshal(target)
 	if err != nil {
 		return nil, fmt.Errorf("Failed to marshall target instance: %s", err)
 	}
-	response, err := c.Post().Resource(api.Resource_Type_Target).
+
+	request := c.Post().Resource(api.Resource_Type_Target).
 		Header("Content-Type", "application/json").
 		Header("Accept", "application/json").
-		Data(targetData).
-		Do()
+		Header("Cookie", fmt.Sprintf("%s=%s", c.SessionCookie.Name, c.SessionCookie.Value)).
+		Data(targetData)
+
+	glog.V(4).Infof("[AddTarget] Request %++v\n", request)
+
+	response, err := request.Do()
 	if err != nil {
 		return nil, fmt.Errorf("Failed to add target: %s", err)
 	}
 	if response.statusCode != 200 {
 		return nil, buildResponseError("target addition", response.status, response.body)
 	}
+
 	return &response, nil
+}
+
+//Find if the given target exists within the Turbo server
+func (c *Client) FindTarget(target *api.Target) (bool, error) {
+	// Get a list of targets from the Turbo server
+	request := c.Get().Resource(api.Resource_Type_Target).
+		Header("Content-Type", "application/json").
+		Header("Accept", "application/json").
+		Header("Cookie", fmt.Sprintf("%s=%s", c.SessionCookie.Name, c.SessionCookie.Value))
+
+	glog.V(4).Infof("[FindTarget] Request %++v\n", request)
+
+	response, err := request.Do()
+
+	if err != nil {
+		fmt.Printf("Failed to execute find target request: %s", err)
+		return false, fmt.Errorf("Failed to execute find target request: %s", err)
+	}
+
+	if response.statusCode != 200 {
+		return false, buildResponseError("find target", response.status, response.body)
+	}
+
+	// The target identifier for the given target
+	targetId := getTargetId(target)
+
+	// Parse the response - list of targets
+	var targetList []interface{}
+	json.Unmarshal([]byte(response.body), &targetList)
+
+	// Iterate over the list of targets to look for the given target
+	// by comparing the category, target type and identifier fields
+	for _, tgt := range targetList {
+		targetInstance, isMap := tgt.(map[string]interface{})
+		if !isMap {
+			continue
+		}
+		var category, targetType, tgtId string
+		category, _ = targetInstance["category"].(string)
+		targetType, _ = targetInstance["type"].(string)
+		inputFields, ok := targetInstance["inputFields"].([]interface{})
+		if ok {
+			// array of InputFields
+			for _, value := range inputFields {
+				// an instance of InputField
+				inputFieldMap, isMap := value.(map[string]interface{})
+				// Get the target identifier value from the
+				// input field named 'targetIdentifier'
+				if isMap {
+					field, isNameField := inputFieldMap["name"].(string)
+					//
+					if isNameField && field == "targetIdentifier" {
+						tgtId, _ = inputFieldMap["value"].(string)
+					}
+				}
+			}
+		}
+		glog.V(4).Infof("%s::%s::%s\n", category, targetType, tgtId)
+		if target.Category == category && target.Type == targetType && tgtId == targetId {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
+// Get the target identifier for the given target
+func getTargetId(target *api.Target) string {
+	for _, inputField := range target.InputFields {
+		field := inputField.Name
+		if field == "targetIdentifier" {
+			tgtId := inputField.Value
+			return tgtId
+		}
+	}
+	return ""
 }
 
 func buildResponseError(requestDesc string, status string, content string) error {
---
+++ vendor/github.com/turbonomic/turbo-go-sdk/pkg/builder/flow_dto_builder.go
@@ -0,0 +1,111 @@
+package builder
+
+import (
+	"errors"
+	"github.com/turbonomic/turbo-go-sdk/pkg/proto"
+)
+
+type Protocol int32
+
+const (
+	TCP Protocol = 1
+	UDP Protocol = 2
+)
+
+type FlowDTOBuilder struct {
+	sourceAddress      string
+	sourcePort         int32
+	destinationAddress string
+	destinationPort    int32
+	protocol           Protocol
+	flowAmount         float64
+	latency            int64
+	rx                 int64
+	tx                 int64
+	err                error
+}
+
+func NewFlowDTOBuilder() *FlowDTOBuilder {
+	return &FlowDTOBuilder{
+		sourcePort:      0,
+		destinationPort: 0,
+		protocol:        TCP,
+		flowAmount:      0,
+		latency:         0,
+		rx:              0,
+		tx:              0,
+	}
+}
+
+// Sets the source
+func (builder *FlowDTOBuilder) Source(source string) *FlowDTOBuilder {
+	builder.sourceAddress = source
+	return builder
+}
+
+// Sets the destination
+func (builder *FlowDTOBuilder) Destination(destination string, port int32) *FlowDTOBuilder {
+	builder.destinationAddress = destination
+	builder.destinationPort = port
+	return builder
+}
+
+// Set the protocol
+func (builder *FlowDTOBuilder) Protocol(protocol Protocol) *FlowDTOBuilder {
+	if protocol != TCP && protocol != UDP {
+		builder.err = errors.New("unsupported protocol")
+		return builder
+	}
+	builder.protocol = protocol
+	return builder
+}
+
+// Sets the flow amount
+func (builder *FlowDTOBuilder) FlowAmount(flowAmount float64) *FlowDTOBuilder {
+	builder.flowAmount = flowAmount
+	return builder
+}
+
+// Sets the latency
+func (builder *FlowDTOBuilder) Latency(latency int64) *FlowDTOBuilder {
+	builder.latency = latency
+	return builder
+}
+
+// Sets the received amount
+func (builder *FlowDTOBuilder) Received(received int64) *FlowDTOBuilder {
+	builder.rx = received
+	return builder
+}
+
+// Sets the transmitted amount
+func (builder *FlowDTOBuilder) Transmitted(transmitted int64) *FlowDTOBuilder {
+	builder.tx = transmitted
+	return builder
+}
+
+// Creates the DTO
+func (builder *FlowDTOBuilder) Create() (*proto.FlowDTO, error) {
+	if builder.err != nil {
+		return nil, builder.err
+	}
+
+	src := &proto.EntityIdentityData{
+		IpAddress: &builder.sourceAddress,
+		Port:      &builder.sourcePort,
+	}
+	dst := &proto.EntityIdentityData{
+		IpAddress: &builder.destinationAddress,
+		Port:      &builder.destinationPort,
+	}
+	protocol := proto.FlowDTO_Protocol(builder.protocol)
+	return &proto.FlowDTO{
+		SourceEntityIdentityData: src,
+		DestEntityIdentityData:   dst,
+		Protocol:                 &protocol,
+		FlowAmount:               &builder.flowAmount,
+		Latency:                  &builder.latency,
+		ReceivedAmount:           &builder.rx,
+		TransmittedAmount:        &builder.tx,
+	}, nil
+}
