3f28c9bda16316a0340edac9862e9457199becc9
irfanurrehman <irfan.rehman@turbonomic.com>
2020-03-23 23:43:44 +1100

    Add bought storage amount commodities to Pod DTOs

--- pkg/discovery/dtofactory/pod_entity_dto_builder_test.go
+++ pkg/discovery/dtofactory/pod_entity_dto_builder_test.go
@@ -3,11 +3,12 @@ package dtofactory
 import (
 	"testing"
 
+	"reflect"
+
 	"github.com/turbonomic/kubeturbo/pkg/discovery/metrics"
 	"github.com/turbonomic/turbo-go-sdk/pkg/proto"
 	api "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"reflect"
 )
 
 var builder = &podEntityDTOBuilder{
