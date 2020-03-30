c889c62607aadbd33ff14235941977e26d21c1b9
irfanurrehman <irfan.rehman@turbonomic.com>
2020-03-30 21:35:28 +1100

    Add volumes to supply chain, set action policies and volume properties

---
+++ pkg/discovery/dtofactory/property/volume_properties.go
@@ -0,0 +1,23 @@
+package property
+
+import (
+	api "k8s.io/api/core/v1"
+
+	"github.com/turbonomic/turbo-go-sdk/pkg/proto"
+)
+
+const (
+	k8sVolumeName = "KubernetesVolumeName"
+)
+
+// Build entity properties for a volume. The name is the name of the volume shown inside Kubernetes cluster.
+func BuildVolumeProperties(vol *api.PersistentVolume) *proto.EntityDTO_EntityProperty {
+	propertyNamespace := k8sPropertyNamespace
+	propertyName := k8sVolumeName
+	propertyValue := vol.Name
+	return &proto.EntityDTO_EntityProperty{
+		Namespace: &propertyNamespace,
+		Name:      &propertyName,
+		Value:     &propertyValue,
+	}
+}
