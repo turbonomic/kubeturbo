474b60dc173bf50613f8dde46af487cb6228d826
Yue Wu <yuriyuewu@gmail.com>
2020-04-16 18:19:36 -0400

    Create new WorkloadController entity for new container model (#401)

--- pkg/registration/registration_client.go
+++ pkg/registration/registration_client.go
@@ -166,6 +166,7 @@ func (rClient *K8sRegistrationClient) GetEntityMetadata() (result []*proto.Entit
 
 	entities := []proto.EntityDTO_EntityType{
 		proto.EntityDTO_NAMESPACE,
+		proto.EntityDTO_WORKLOAD_CONTROLLER,
 		proto.EntityDTO_VIRTUAL_MACHINE,
 		proto.EntityDTO_CONTAINER_SPEC,
 		proto.EntityDTO_CONTAINER_POD,
