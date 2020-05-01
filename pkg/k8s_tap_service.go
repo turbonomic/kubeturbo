7a96c53ea0deb4473cac6ffee04f8657d63ae212
Enlin Xu <jannleno1@gmail.com>
2020-05-01 15:16:37 -0400

    Revert "Add probe UI category"
    This reverts commit 6ac42736566c024857a5488785cfb5aa44dbbbe8.

--- pkg/k8s_tap_service.go
+++ pkg/k8s_tap_service.go
@@ -170,8 +170,7 @@ func NewKubernetesTAPService(config *Config) (*K8sTAPService, error) {
 	// Kubernetes Probe Action Execution Client
 	actionHandler := action.NewActionHandler(actionHandlerConfig)
 
-	probeBuilder := probe.NewProbeBuilder(config.tapSpec.TargetType,
-		config.tapSpec.ProbeCategory, config.tapSpec.ProbeUICategory).
+	probeBuilder := probe.NewProbeBuilder(config.tapSpec.TargetType, config.tapSpec.ProbeCategory).
 		WithDiscoveryOptions(probe.FullRediscoveryIntervalSecondsOption(int32(config.DiscoveryIntervalSec))).
 		RegisteredBy(registrationClient).
 		WithActionPolicies(registrationClient).
