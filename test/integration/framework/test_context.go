0686b209a8c18e414c1abd32f6b7b0b4a7377f48
irfanurrehman <irfan.rehman@turbonomic.com>
2021-01-11 21:45:59 +1100

    Setup klog and glog flags in integration test to ensure loglevels could be set for both

--- test/integration/framework/test_context.go
+++ test/integration/framework/test_context.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"time"
 
+	"github.com/golang/glog"
+
 	"k8s.io/klog"
 )
 
@@ -36,7 +38,43 @@ func validateFlags(t *TestContextType) {
 }
 
 func ParseFlags() {
-	registerFlags(TestContext)
-	flag.Parse()
+	initFlags(TestContext)
 	validateFlags(TestContext)
+}
+
+func initFlags(t *TestContextType) {
+	// These arguments can be overwritten from the command-line args
+	_ = flag.Set("alsologtostderr", "true")
+	registerFlags(t)
+	flag.Parse()
+
+	// Initialize klog specific flags into a new FlagSet
+	klogFlags := flag.NewFlagSet("klog", flag.ContinueOnError)
+	klog.InitFlags(klogFlags)
+
+	// Add klog specific flags into goflag
+	klogFlags.VisitAll(func(klogFlag *flag.Flag) {
+		if flag.CommandLine.Lookup(klogFlag.Name) == nil {
+			// This is a klog specific flag
+			flag.CommandLine.Var(klogFlag.Value, klogFlag.Name, klogFlag.Usage)
+		}
+	})
+
+	// Add log flush frequency
+	flag.Duration("log-flush-frequency", 5*time.Second,
+		"Maximum number of seconds between log flushes")
+
+	// Sync the glog and klog flags
+	flag.CommandLine.VisitAll(func(glogFlag *flag.Flag) {
+		klogFlag := klogFlags.Lookup(glogFlag.Name)
+		if klogFlag != nil {
+			value := glogFlag.Value.String()
+			_ = klogFlag.Value.Set(value)
+		}
+	})
+
+	// Print out all parsed flags
+	flag.VisitAll(func(flag *flag.Flag) {
+		glog.V(2).Infof("FLAG: --%s=%q", flag.Name, flag.Value)
+	})
 }
