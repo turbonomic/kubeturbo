3c1046e93f0d0dc6850f74f7b60ef8ba6f266f04
Cheuk Lam <chlam4@hotmail.com>
2022-02-04 16:21:24 -0500

    Discovering taints as node tags and tolerations as pod tags.

--- pkg/discovery/dtofactory/property/node_properties.go
+++ pkg/discovery/dtofactory/property/node_properties.go
@@ -1,12 +1,16 @@
 package property
 
 import (
-	api "k8s.io/api/core/v1"
-
 	"github.com/turbonomic/turbo-go-sdk/pkg/proto"
+	api "k8s.io/api/core/v1"
 )
 
-// Build entity properties for a node. The name is the name of the node shown inside Kubernetes cluster.
+const TaintPropertyValueSuffix = "[KUBERNETES TAINT]"
+
+// BuildNodeProperties builds entity properties for a node. It brings over the following 3 things as properties:
+// 1. The name of the node shown inside Kubernetes cluster; the property name is "KubernetesNodeName".
+// 2. The labels of the node; each label's key-value pair is directly brought over as tags.
+// 3. The taints of the node.
 func BuildNodeProperties(node *api.Node) []*proto.EntityDTO_EntityProperty {
 	var properties []*proto.EntityDTO_EntityProperty
 	propertyNamespace := k8sPropertyNamespace
@@ -24,6 +28,19 @@ func BuildNodeProperties(node *api.Node) []*proto.EntityDTO_EntityProperty {
 	for label, lval := range labels {
 		tagNamePropertyName := label
 		tagNamePropertyValue := lval
+		tagProperty := &proto.EntityDTO_EntityProperty{
+			Namespace: &tagsPropertyNamespace,
+			Name:      &tagNamePropertyName,
+			Value:     &tagNamePropertyValue,
+		}
+		properties = append(properties, tagProperty)
+	}
+	for _, taint := range node.Spec.Taints {
+		tagNamePropertyName := taint.Key
+		tagNamePropertyValue := string(taint.Effect) + " " + TaintPropertyValueSuffix
+		if taint.Value != "" {
+			tagNamePropertyValue = taint.Value + " " + tagNamePropertyValue
+		}
 		tagProperty := &proto.EntityDTO_EntityProperty{
 			Namespace: &tagsPropertyNamespace,
 			Name:      &tagNamePropertyName,
--- pkg/discovery/dtofactory/property/property_test.go
+++ pkg/discovery/dtofactory/property/property_test.go
@@ -1,16 +1,41 @@
 package property
 
 import (
+	"github.com/stretchr/testify/assert"
 	api "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"testing"
 )
 
+const (
+	label1Key   = "label1"
+	label1Value = "value1"
+	label2Key   = "label2"
+	label2Value = "value2"
+
+	taintAKey    = "taintA"
+	taintAEffect = api.TaintEffectNoSchedule
+	taintBKey    = "taintB"
+	taintBValue  = "foo"
+	taintBEffect = api.TaintEffectNoExecute
+	taintCKey    = "taintC"
+	taintCValue  = "bar"
+	taintCEffect = api.TaintEffectPreferNoSchedule
+
+	toleration1Key    = taintAKey
+	toleration1Op     = api.TolerationOpExists
+	toleration1Effect = taintAEffect
+	toleration2Key    = taintBKey
+	toleration2Op     = api.TolerationOpEqual
+	toleration2Value  = taintBValue
+	toleration2Effect = taintBEffect
+)
+
 func TestNodeProperty(t *testing.T) {
 	labels := make(map[string]string)
-	labels["label1"] = "value1"
-	labels["label2"] = "valuel2"
+	labels[label1Key] = label1Value
+	labels[label2Key] = label2Value
 
 	node := &api.Node{
 		TypeMeta: metav1.TypeMeta{
@@ -23,6 +48,25 @@ func TestNodeProperty(t *testing.T) {
 			UID:    "my-node-1-UID",
 			Labels: labels,
 		},
+
+		Spec: api.NodeSpec{
+			Taints: []api.Taint{
+				{
+					Key:    taintAKey,
+					Effect: taintAEffect,
+				},
+				{
+					Key:    taintBKey,
+					Value:  taintBValue,
+					Effect: taintBEffect,
+				},
+				{
+					Key:    taintCKey,
+					Value:  taintCValue,
+					Effect: taintCEffect,
+				},
+			},
+		},
 	}
 
 	ps := BuildNodeProperties(node)
@@ -31,12 +75,36 @@ func TestNodeProperty(t *testing.T) {
 	if nodeName != node.Name {
 		t.Errorf("Failed to get node name from perperties: %+v", ps)
 	}
+
+	matches := 0
+	for _, p := range ps {
+		if p.GetNamespace() == VCTagsPropertyNamespace {
+			var expected string
+			switch p.GetName() {
+			case label1Key:
+				expected = label1Value
+			case label2Key:
+				expected = label2Value
+			case taintAKey:
+				expected = string(taintAEffect) + " " + TaintPropertyValueSuffix
+			case taintBKey:
+				expected = taintBValue + " " + string(taintBEffect) + " " + TaintPropertyValueSuffix
+			case taintCKey:
+				expected = taintCValue + " " + string(taintCEffect) + " " + TaintPropertyValueSuffix
+			default:
+				continue
+			}
+			matches++
+			assert.EqualValues(t, expected, p.GetValue())
+		}
+	}
+	assert.Equal(t, 5, matches, "there should be 5 matches in the test node properties")
 }
 
 func TestBuildPodProperties(t *testing.T) {
 	labels := make(map[string]string)
-	labels["label1"] = "value1"
-	labels["label2"] = "valuel2"
+	labels[label1Key] = label1Value
+	labels[label2Key] = label2Value
 
 	pod := &api.Pod{
 		TypeMeta: metav1.TypeMeta{
@@ -49,6 +117,21 @@ func TestBuildPodProperties(t *testing.T) {
 			Namespace: "my-namespace",
 			UID:       "my-pod-1-UID",
 			Labels:    labels,
+		},
+		Spec: api.PodSpec{
+			Tolerations: []api.Toleration{
+				{
+					Key:      toleration1Key,
+					Operator: toleration1Op,
+					Effect:   toleration1Effect,
+				},
+				{
+					Key:      toleration2Key,
+					Operator: toleration2Op,
+					Value:    toleration2Value,
+					Effect:   toleration2Effect,
+				},
+			},
 		},
 	}
 
@@ -66,6 +149,28 @@ func TestBuildPodProperties(t *testing.T) {
 	if name != pod.Name {
 		t.Errorf("Pod property test failed: pod name is wrong: (%v) Vs. (%v)", name, pod.Name)
 	}
+
+	matches := 0
+	for _, p := range ps {
+		if p.GetNamespace() == VCTagsPropertyNamespace {
+			var expected string
+			switch p.GetName() {
+			case label1Key:
+				expected = label1Value
+			case label2Key:
+				expected = label2Value
+			case toleration1Key:
+				expected = string(toleration1Op) + " " + string(toleration1Effect) + " " + TolerationPropertyValueSuffix
+			case toleration2Key:
+				expected = toleration2Value + " " + string(toleration2Op) + " " + string(toleration2Effect) + " " + TolerationPropertyValueSuffix
+			default:
+				continue
+			}
+			matches++
+			assert.EqualValues(t, expected, p.GetValue())
+		}
+	}
+	assert.Equal(t, 4, matches, "there should be 4 matches in the test pod properties")
 }
 
 func TestAddHostingPodProperties(t *testing.T) {
