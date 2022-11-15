29c5365d24c5baabcf14026b2d0c91a26b718f19
sumanthkodali <sumanth.kodali@ibm.com>
2022-11-15 15:56:45 -0600

    Integration test

--- test/integration/discovery.go
+++ test/integration/discovery.go
@@ -251,14 +251,11 @@ var _ = Describe("Discover Cluster", func() {
 				if pod.GetPowerState() != proto.EntityDTO_POWERSTATE_UNKNOWN {
 					framework.Failf("All pods with NotReady Node provider should be in an unknown state")
 				}
-				if pod.GetPowerState() != proto.EntityDTO_POWERED_ON {
-					framework.Failf("All pods with NotReady Node provider should be in an unknown state")
+				if !pod.ActionEligibility.GetSuspendable() {
+					framework.Failf("All pods with NotReady Node provider is not eligible to suspended")
 				}
-				if pod.ActionEligibility.GetSuspendable() {
-					framework.Failf("All pods with NotReady Node provider should be in an unknown state")
-				}
-				if pod.ActionEligibility.GetCloneable() {
-					framework.Failf("All pods with NotReady Node provider should be in an unknown state")
+				if !pod.ActionEligibility.GetCloneable() {
+					framework.Failf("All pods with NotReady Node provider is not eligible to provisioned")
 				}
 			}
 		})
