c9bc74c788013265e971fa5e75201b52b47ef863
sumanthkodali <sumanth.kodali@ibm.com>
2022-11-17 10:22:33 -0600

    Removal of a check for inactive state as per comments for NotReady state pod

--- test/integration/discovery.go
+++ test/integration/discovery.go
@@ -257,9 +257,6 @@ var _ = Describe("Discover Cluster", func() {
 				if pod.GetPowerState() != proto.EntityDTO_POWERSTATE_UNKNOWN {
 					framework.Failf("All pods with NotReady Node provider should be in an unknown state")
 				}
-				if pod.GetPowerState() == proto.EntityDTO_POWERED_ON {
-					framework.Failf("All pods with NotReady Node provider should be in inactive state")
-				}
 			}
 		})
 
