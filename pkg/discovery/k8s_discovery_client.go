aa13da2a4df722d3c989a9ec9b4a06fb6d0770ef
Irfan Ur Rehman <irfan.rehman@turbonomic.com>
2023-01-06 19:31:05 +1100

    Add timing information to discovery stages for benchmarking

--- pkg/discovery/k8s_discovery_client.go
+++ pkg/discovery/k8s_discovery_client.go
@@ -307,10 +307,12 @@ func (dc *K8sDiscoveryClient) Discover(
 // DiscoverWithNewFramework performs the actual discovery.
 func (dc *K8sDiscoveryClient) DiscoverWithNewFramework(targetID string) ([]*proto.EntityDTO, []*proto.GroupDTO, error) {
 	// CREATE CLUSTER, NODES, NAMESPACES, QUOTAS, SERVICES HERE
+	start := time.Now()
 	clusterSummary, err := dc.clusterProcessor.DiscoverCluster()
 	if err != nil {
 		return nil, nil, fmt.Errorf("failed to process cluster: %v", err)
 	}
+	glog.V(3).Infof("Discovering cluster resources took %s", time.Since(start))
 
 	// Cache operatorResourceSpecMap in ormClient
 	numCRs := dc.Config.OrmClient.CacheORMSpecMap()
@@ -325,17 +327,20 @@ func (dc *K8sDiscoveryClient) DiscoverWithNewFramework(targetID string) ([]*prot
 	// Stops scheduling dispatcher to assign sampling discovery tasks.
 	dc.samplingDispatcher.FinishSampling()
 
+	start = time.Now()
 	// Discover pods and create DTOs for nodes, namespaces, controllers, pods, containers, application.
 	// Merge collected usage data samples from globalEntityMetricSink into the metric sink of each individual discovery worker.
 	// Collect the kubePod, kubeNamespace metrics, groups and kubeControllers from all the discovery workers.
 	taskCount := dc.dispatcher.Dispatch(nodes, clusterSummary)
 	result := dc.resultCollector.Collect(taskCount)
+	glog.V(3).Infof("Collection and processing of metrics from node kubelets took %s", time.Since(start))
 
 	// Clear globalEntityMetricSink cache after collecting full discovery results
 	dc.globalEntityMetricSink.ClearCache()
 	// Reschedule dispatch sampling discovery tasks for newly discovered nodes
 	dc.samplingDispatcher.ScheduleDispatch(nodes)
 
+	start = time.Now()
 	// Namespace discovery worker to create namespace DTOs
 	stitchType := dc.Config.probeConfig.StitchingPropertyType
 	namespacesDiscoveryWorker := worker.Newk8sNamespaceDiscoveryWorker(clusterSummary, stitchType)
@@ -398,10 +403,12 @@ func (dc *K8sDiscoveryClient) DiscoverWithNewFramework(targetID string) ([]*prot
 	result.EntityDTOs = append(result.EntityDTOs, businessAppEntityDTOBuilderEntityDTOs...)
 
 	glog.V(2).Infof("There are totally %d entityDTOs.", len(result.EntityDTOs))
+	glog.V(3).Infof("Postprocessing and aggregatig DTOs took %s", time.Since(start))
 
 	// affinity process
 	if !utilfeature.DefaultFeatureGate.Enabled(features.IgnoreAffinities) {
 		glog.V(2).Infof("Begin to process affinity.")
+		start := time.Now()
 		affinityProcessor, err := compliance.NewAffinityProcessor(clusterSummary)
 		if err != nil {
 			glog.Errorf("Failed during process affinity rules: %s", err)
@@ -409,18 +416,21 @@ func (dc *K8sDiscoveryClient) DiscoverWithNewFramework(targetID string) ([]*prot
 			result.EntityDTOs = affinityProcessor.ProcessAffinityRules(result.EntityDTOs)
 		}
 		glog.V(2).Infof("Successfully processed affinity.")
+		glog.V(3).Infof("Processing affinities took %s", time.Since(start))
 	} else {
 		glog.V(2).Infof("Ignoring affinities.")
 	}
 
 	// Taint-toleration process to create access commodities
 	glog.V(2).Infof("Begin to process taints and tolerations")
+	start = time.Now()
 	taintTolerationProcessor, err := compliance.NewTaintTolerationProcessor(clusterSummary)
 	if err != nil {
 		glog.Errorf("Failed during process taints and tolerations: %v", err)
 	} else {
 		// Add access commodities to entity DOTs based on the taint-toleration rules
 		taintTolerationProcessor.Process(result.EntityDTOs)
+		glog.V(3).Infof("Processing taints took %s", time.Since(start))
 	}
 
 	glog.V(2).Infof("Successfully processed taints and tolerations.")
