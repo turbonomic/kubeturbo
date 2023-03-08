54718497684170179a8481744fa4cad961330ac7
sumanthkodali <sumanth.kodali@ibm.com>
2023-03-08 15:15:45 -0500

    add test cases for IP stitching

--- pkg/discovery/dtofactory/service_entity_dto_builder_test.go
+++ pkg/discovery/dtofactory/service_entity_dto_builder_test.go
@@ -1,10 +1,14 @@
 package dtofactory
 
 import (
+	"testing"
+
 	"github.com/stretchr/testify/assert"
+	"github.com/turbonomic/kubeturbo/pkg/discovery/stitching"
 	"github.com/turbonomic/turbo-go-sdk/pkg/proto"
 	api "k8s.io/api/core/v1"
-	"testing"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 )
 
 var testService = api.Service{
@@ -14,6 +18,12 @@ var testService = api.Service{
 	},
 }
 
+func createPods() []*api.Pod {
+	pod1 := &api.Pod{Status: api.PodStatus{PodIP: "10.1.1.1"}, ObjectMeta: metav1.ObjectMeta{UID: types.UID("1234-456-789")}}
+	pod2 := &api.Pod{Status: api.PodStatus{PodIP: "10.1.1.2"}, ObjectMeta: metav1.ObjectMeta{UID: types.UID("5678-1223-1122")}}
+	pod3 := &api.Pod{Status: api.PodStatus{PodIP: "10.1.1.3"}, ObjectMeta: metav1.ObjectMeta{UID: types.UID("5636-7799-8838")}}
+	return []*api.Pod{pod1, pod2, pod3}
+}
 func TestBuildServiceData(t *testing.T) {
 	serviceData := createServiceData(&testService)
 	assert.Equal(t, testService.Spec.ClusterIP, *serviceData.IpAddress)
@@ -22,4 +32,48 @@ func TestBuildServiceData(t *testing.T) {
 	assert.Equal(t,
 		proto.EntityDTO_KubernetesServiceData_ServiceType(serviceTypeEnum),
 		serviceData.GetKubernetesServiceData().GetServiceType())
+}
+
+func TestGetIPProperty(t *testing.T) {
+	pod := createPods()
+	// Call the function
+	result := getIPProperty(pod)
+
+	// Check the result
+	expectedNamespace := stitching.DefaultPropertyNamespace
+	expectedAttribute := stitching.AppStitchingAttr
+	expectedValue := "Service-10.1.1.1-1234,Service-10.1.1.2-5678,Service-10.1.1.3-5636"
+
+	if result.Namespace == nil || *result.Namespace != expectedNamespace {
+		t.Errorf("IP property test failed: namespace is incorrect (%v) Vs. (%v)", result.Namespace, expectedNamespace)
+	}
+	if result.Name == nil || *result.Name != expectedAttribute {
+		t.Errorf("IP property test failed: name is incorrect (%v) Vs. (%v)", result.Name, expectedAttribute)
+	}
+	if result.Value == nil || *result.Value != expectedValue {
+		t.Errorf("IP property test failed: value is incorrect (%v) Vs. (%v)", result.Value, expectedValue)
+	}
+}
+
+func TestGetIPPropertyEmptyInput(t *testing.T) {
+	// Create empty input slice of pods
+	pods := []*api.Pod{}
+
+	// Call the function
+	result := getIPProperty(pods)
+
+	// Check the result
+	expectedNamespace := stitching.DefaultPropertyNamespace
+	expectedAttribute := stitching.AppStitchingAttr
+	expectedValue := ""
+
+	if result.Namespace == nil || *result.Namespace != expectedNamespace {
+		t.Errorf("IP property test failed: namespace is incorrect (%v) Vs. (%v)", result.Namespace, expectedNamespace)
+	}
+	if result.Name == nil || *result.Name != expectedAttribute {
+		t.Errorf("IP property test failed: name is incorrect (%v) Vs. (%v)", result.Name, expectedAttribute)
+	}
+	if result.Value == nil || *result.Value != expectedValue {
+		t.Errorf("IP property test failed: value is incorrect (%v) Vs. (%v)", result.Value, expectedValue)
+	}
 }
